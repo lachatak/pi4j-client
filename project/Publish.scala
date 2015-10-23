7a84f0f73a4fa27bbdf2fff683a23278e0e14281
Krisztian Lachata <krisztian.lachata@gmail.com>
2015-10-23 23:37:38 +0100

    make the project sonatype releaseble

---
+++ project/Publish.scala
@@ -0,0 +1,44 @@
+import sbt.Keys._
+import sbt._
+import xerial.sbt.Sonatype.SonatypeKeys._
+
+object Publish {
+
+  lazy val settings = Seq(
+    profileName := "lachatak",
+    homepage := Some(url("https://github.com/lachatak/pi4j-client")),
+    publishMavenStyle := true,
+    publishArtifact in Test := false,
+    pomIncludeRepository := { _ => false},
+    credentials += Credentials(Path.userHome / ".sbt" / ".credentials"),
+    pomExtra := (
+      <scm>
+        <url>git@github.com:lachatak/pi4j-client.git</url>
+        <connection>scm:git:git@github.com:lachatak/pi4j-client.git</connection>
+      </scm>
+        <developers>
+          <developer>
+            <id>lachatak</id>
+            <name>Krisztian Lachata</name>
+            <email>krisztian.lachata@gmail.com</email>
+            <url>http://kaloz.org</url>
+          </developer>
+          <developer>
+            <id>polgart</id>
+            <name>Tamas Polgar</name>
+            <email>tms.polgar@gmail.com</email>
+          </developer>
+        </developers>),
+    publishTo := {
+      val nexus = "https://oss.sonatype.org/"
+      if (isSnapshot.value)
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases" at nexus + "service/local/staging/deploy/maven2")
+    })
+
+  lazy val noPublishing = Seq(
+    publish :=(),
+    publishLocal :=()
+  )
+}
---
+++ project/Release.scala
@@ -0,0 +1,16 @@
+import scala.util.Properties.{ propOrEmpty, propOrNone }
+
+import com.typesafe.sbt.SbtPgp.PgpKeys._
+import sbtrelease.ReleasePlugin._
+import sbtrelease.ReleasePlugin.ReleaseKeys._
+
+object Release {
+
+  lazy val settings = releaseSettings ++ Seq(
+    publishArtifactsAction := publishSigned.value,
+    releaseVersion := { _ => propOrEmpty("releaseVersion")},
+    nextVersion := { _ => propOrEmpty("developmentVersion")},
+    pgpPassphrase := propOrNone("gpg.passphrase").map(_.toCharArray)
+  )
+
+}
