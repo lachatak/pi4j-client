17804e690e8b428e533119079cda95d42cfee81b
menza <menyuske@gmail.com>
2015-11-02 22:54:55 +0000

    merging code

--- web-client/src/main/scala/org/kaloz/pi4j/client/web/WebApp.scala
+++ web-client/src/main/scala/org/kaloz/pi4j/client/web/WebApp.scala
@@ -5,9 +5,13 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.server.Directives._
 import akka.stream._
+import akka.pattern.ask
 import akka.stream.scaladsl._
+import akka.util.Timeout
 import org.json4s.DefaultFormats
+import org.kaloz.pi4j.client.actor.InMemoryClientActor
 import org.kaloz.pi4j.client.web.SinkActor.RegisterSink
+import org.kaloz.pi4j.common.messages.ClientMessages.GpioMessages.WiringPiSetupRequest
 import scala.concurrent.duration._
 
 
@@ -16,17 +20,17 @@ import scala.io.StdIn
 object WebApp extends App {
 
   implicit val formats = DefaultFormats
-  implicit val actorSystem = ActorSystem("akka-system")
+  implicit val system = ActorSystem("web-client-actor-system")
+  private val sinkActor = system.actorOf(Props[SinkActor])
+  private val webClientActor = system.actorOf(InMemoryClientActor.props((x,y) => sinkActor), "webClientActor")
   implicit val flowMaterializer = ActorMaterializer()
 
   val interface = "localhost"
   val port = 8080
 
   val myFlow: Flow[Message, Message, _] = {
-    val sinkActor = actorSystem.actorOf(Props[SinkActor])
-
-    val in = Flow[Message].to(Sink.actorRef(sinkActor, "COMPLETE-MESSAGE"))
-    val out = Source.actorRef(10, OverflowStrategy.fail).mapMaterializedValue(sinkActor ! RegisterSink(_))
+    val in = Flow[Message].to(Sink.actorRef(sinkActor, "onCompleteMessage"))
+    val out = Source.actorRef(10, OverflowStrategy.fail).mapMaterializedValue(sinkActor ! RegisterSink(_, webClientActor))
 
     Flow.wrap(in, out)(Keep.both)
   }
@@ -44,9 +48,9 @@ object WebApp extends App {
   println(s"Server is now online at http://$interface:$port\nPress RETURN to stop...")
   StdIn.readLine()
 
-  import actorSystem.dispatcher
+  import system.dispatcher
 
-  binding.flatMap(_.unbind()).onComplete(_ => actorSystem.shutdown())
+  binding.flatMap(_.unbind()).onComplete(_ => system.terminate())
   println("Server is down...")
 }
 
@@ -54,8 +58,10 @@ class SinkActor extends Actor {
   import SinkActor._
   import scala.concurrent.ExecutionContext.Implicits.global
 
+  implicit val timeout = Timeout(5 seconds)
   def receive = {
-    case RegisterSink(actorRef) => {
+    case RegisterSink(actorRef, ar2) => {
+      (ar2 ? WiringPiSetupRequest).foreach(println)
       context.system.scheduler.schedule(0 second, 5 seconds, actorRef, TextMessage("""{"type": "pinStateChange", "pinId": 1, "pinValue": "out-high"}"""))
 //      context.become(receiveWithSink(actorRef))
     }
@@ -67,5 +73,5 @@ class SinkActor extends Actor {
 }
 
 object SinkActor {
-  case class RegisterSink(actorRef: ActorRef)
+  case class RegisterSink(actorRef: ActorRef, actorRef2: ActorRef)
 }
